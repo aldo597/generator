658ff7f19213370ec7df1b1cc0b7257f658666fe
aldo597 <cataldo.caruso@ep.europa.eu>
2026-09-17 22:18:14 +0200

    Save local version

--- frontend/api.js
+++ frontend/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 // Base URL: Railway Deployment
-// const BASE = "https://generatorbackend-new-project.up.railway.app/";
-const BASE = "http://127.0.0.1:8000";
+const BASE = "https://generatorbackend-new-project.up.railway.app/";
+// const BASE = "http://127.0.0.1:8000";
 
 const api = axios.create({
   baseURL: BASE,
