f40adee260498e834399b6d0c6b3b5a6c0bd14ae
aldo597 <cataldo.caruso@ep.europa.eu>
2026-09-18 10:43:38 +0200

    p

--- frontend/api.js
+++ frontend/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 // Base URL: Railway Deployment
-const BASE = "https://generatorbackend-new-project.up.railway.app/";
+const BASE = "https://generatorbackend-production-5575.up.railway.app/";
+
 // const BASE = "http://127.0.0.1:8000";
 
 const api = axios.create({
